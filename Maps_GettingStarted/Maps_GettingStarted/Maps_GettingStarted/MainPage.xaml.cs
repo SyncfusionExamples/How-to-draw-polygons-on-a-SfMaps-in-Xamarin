0b9b69fd7668b8ad9ba87e0cb715c70f9247e041
AzureAD\HemalathaMarikumar <hemalatha.marikumar@syncfusion.com>
2020-07-08 18:39:28 +0530

    Included sample

---
+++ Maps_GettingStarted/Maps_GettingStarted/Maps_GettingStarted/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Syncfusion.SfMaps.XForms;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Maps_GettingStarted
+{
+    public partial class MainPage : ContentPage
+    {
+        ViewModel model = new ViewModel();
+        public MainPage()
+        {
+            InitializeComponent();
+            this.BindingContext = model;
+        }
+    }
+}
